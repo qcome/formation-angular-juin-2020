5cfcf26d0dc799c632d5a868c7ebe7bcbcac9353
Quentin <quentin.come@etu.univ-orleans.fr>
2020-06-15 15:18:23 +0200

    ajout css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginModule } from './login/login.module';
 import { CoreModule } from './core/core.module';
 import { IconsModule } from './icons/icons.module';
+import { LoginModule } from './login/login.module';
+import { TemplatesModule } from './templates/templates.module';
 import { TextModule } from './text/text.module';
 import { UiModule } from './ui/ui.module';
-import { TemplatesModule } from './templates/templates.module';
+
 
 @NgModule({
   declarations: [
