d814f019c562b1eed693f7ce7d46345346b6ef53
Quentin <quentin.come@etu.univ-orleans.fr>
2020-06-15 14:23:28 +0200

    generate and import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { CoreModule } from './core/core.module';
+import { IconsModule } from './icons/icons.module';
+import { TextModule } from './text/text.module';
+import { UiModule } from './ui/ui.module';
+import { TemplatesModule } from './templates/templates.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule,
+    CoreModule,
+    IconsModule,
+    TextModule,
+    UiModule,
+    TemplatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
